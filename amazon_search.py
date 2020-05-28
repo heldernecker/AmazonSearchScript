00dbe62a35ea8ff76b4039cd6b55f1cc7c97cba2
heldernecker <sumeck@gmail.com>
2020-05-27 23:16:35 -0700

    Added a few lines that will ask the user for a input if none is given by command line

--- amazon_search.py
+++ amazon_search.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 import sys
 
+search_term = ""
+
+try:
+	search_term = sys.argv[1]
+except:
+	while not search_term:
+		print("Please enter a term to be searched: ")
+		search_term = input()
+
 driver = webdriver.Firefox()
 driver.get('https://www.amazon.ca/')
-search_term = sys.argv[1]
 
 searchbox = driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]')
-
 searchbox.send_keys(search_term)
 
 searchButton = driver.find_element_by_xpath(
 	'/html/body/div[1]/header/div/div[1]/div[3]/div/form/div[2]/div/input')
-
 searchButton.click()
